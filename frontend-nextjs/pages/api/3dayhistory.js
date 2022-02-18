69b57d062c7d82b7f4dc359b2fd537ddd5d5e0fc
Jeff Frankl <jfrankl@azavea.com>
2022-02-18 06:16:57 -0500

    Remove 11ty

--- frontend-nextjs/pages/api/3dayhistory.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import data from "/data/id_logit_3hr.js";
-
-export default function handler(req, res) {
-  res.status(200).json(data);
-}
