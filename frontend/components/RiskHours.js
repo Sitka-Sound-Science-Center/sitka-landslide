bb545f77293a14f1166c8581b3ab5d88184f7cec
Jeff Frankl <jfrankl@azavea.com>
2022-04-20 11:29:37 -0400

    Hide 24 hour chart on low risk

--- frontend/components/RiskHours.js
+++ frontend/components/RiskHours.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 import AreaChart from "/components/AreaChart";
 import Risk from "/components/Risk";
@@ -7,29 +8,52 @@ import styles from "/styles/RiskHours.module.css";
 import riskDefinitions from "/content/riskDefinitions";
 
 const RiskHours = ({ hours, riskLevel }) => {
-  const riskSlug = riskDefinitions[riskLevel].slug;
+  const [chartVisible, setChartVisible] = useState(false);
+  const toggleChart = () => {
+    setChartVisible(!chartVisible);
+  };
+
+  const generateRiskMessage = (riskLevel) => {
+    const riskSlug = riskDefinitions[riskLevel].slug;
+
+    if (riskLevel === 0) {
+      return (
+        <span>
+          {riskDefinitions[riskLevel].text} risk for the next 24 hours.{" "}
+          <button onClick={toggleChart} className={styles.showChart}>
+            {chartVisible ? "Hide" : "Show"}&nbsp;chart
+          </button>
+        </span>
+      );
+    } else {
+      return (
+        <span>
+          Sitka will experience {riskDefinitions[riskLevel].text.toLowerCase()} landslide risk
+          within the next 24 hours.{" "}
+          <Link prefetch={false} href={`/prepare/#${riskSlug}`}>
+            <a className={styles.prepare}> How to prepare</a>
+          </Link>
+        </span>
+      );
+    }
+  };
+
+  const riskMessage = generateRiskMessage(riskLevel);
 
   return (
     <section className={styles.section}>
       <div className={styles.header}>
         <h2 className={styles.title}>24 hour forecast</h2>
         <p className={styles.message}>
-          {riskLevel !== 0 && <Risk riskLevel={riskLevel} hasText={false} />}
-          <span>
-            Sitka will experience {riskDefinitions[riskLevel].text.toLowerCase()} risk within the
-            next 24 hours.
-            {riskLevel !== 0 && (
-              <span>
-                <Link prefetch={false} href={`/prepare/#${riskSlug}`}>
-                  <a className={styles.prepare}> Learn how to prepare</a>
-                </Link>
-                .
-              </span>
-            )}
-          </span>
+          <Risk riskLevel={riskLevel} hasText={false} />
+          {riskMessage}
         </p>
       </div>
-      <AreaChart data={hours} />
+      {(chartVisible || riskLevel !== 0) && (
+        <div className={styles.chartContainer}>
+          <AreaChart data={hours} />
+        </div>
+      )}
     </section>
   );
 };
