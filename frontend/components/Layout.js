e4e9aee794b71a426b8e649731ccb516ad0464d2
Jeff Frankl <jfrankl@azavea.com>
2022-05-03 15:34:11 -0400

    Add temporary banner for beta test

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,9 +1,11 @@
+import BetaBanner from "./BetaBanner";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <>
+      <BetaBanner />
       <Header />
       <main>{children}</main>
       <Footer />
