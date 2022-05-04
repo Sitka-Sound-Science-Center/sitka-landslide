795d735e9e7bcfc8416324fbb481922edbdcab9f
Jeff Frankl <jfrankl@azavea.com>
2022-05-04 09:56:09 -0400

    Add temporary banner for beta test (#35)
    * Add temporary banner for beta test
    
    * Remove unused css
    
    * Use less magical background color

---
+++ frontend/components/BetaBanner.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import Icon from "./Icon";
+import { useState, useEffect } from "react";
+
+import styles from "../styles/BetaBanner.module.css";
+
+const BetaBanner = ({ permalink }) => {
+  const [bannerVisibility, setBannerVisibility] = useState(true);
+
+  function hideBanner() {
+    setBannerVisibility(false);
+  }
+
+  return (
+    bannerVisibility && (
+      <a className={styles.banner} href={permalink}>
+        <span className={styles.title}>
+          <Icon name="circle-exclamation" />
+          <span>
+            <strong>Private beta:&nbsp;</strong>not for risk assessment use (live&nbsp;data)
+          </span>
+        </span>
+        <button onClick={hideBanner} className={styles.link}>
+          Close
+        </button>
+      </a>
+    )
+  );
+};
+
+export default BetaBanner;
