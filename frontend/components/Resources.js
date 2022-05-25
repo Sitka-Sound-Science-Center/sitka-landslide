210956c68f7cff8a2b5b0f54572fb40bf676a1aa
Jeff Frankl <jfrankl@azavea.com>
2022-05-25 16:50:09 -0400

    Use index instead of hardcoding id, fix color order

--- frontend/components/Resources.js
+++ frontend/components/Resources.js
@@ -3,37 +3,31 @@ import styles from "/styles/Resources.module.css";
 
 const data = [
   {
-    id: 3,
     title: "Oral histories",
     description: "Explore stories of landslides",
     permalink: "/oral-histories/",
   },
   {
-    id: 2,
     title: "Landslide 101",
     description: "Find out more about the science behind landslides",
     permalink: "/landslide-101/",
   },
   {
-    id: 1,
     title: "Areas at risk",
     description: "Identify where you live relative to landslide susceptibility in Sitka",
     permalink: "/areas-at-risk/",
   },
   {
-    id: 0,
     title: "How to prepare for a landslide",
     description: "Learn what to do before a landslide occurs",
     permalink: "/prepare/",
   },
   {
-    id: 4,
     title: "Published work",
     description: "Access articles published by our research team and related to this work",
     permalink: "/published/",
   },
   {
-    id: 5,
     title: "Report a landslide",
     description: "Share new observations of landslides to contribute to ongoing research",
     permalink: "/report/",
@@ -41,9 +35,9 @@ const data = [
 ];
 
 const Resources = ({ more }) => {
-  const resources = data.map((resource) => (
-    <Link key={resource.id} href={resource.permalink} prefetch={false}>
-      <a className={styles.resource} style={{ backgroundColor: `var(--resource${resource.id})` }}>
+  const resources = data.map((resource, index) => (
+    <Link key={resource.title} href={resource.permalink} prefetch={false}>
+      <a className={styles.resource} style={{ backgroundColor: `var(--resource${index})` }}>
         {more ? (
           <h4 className={styles.title}>{resource.title}</h4>
         ) : (
