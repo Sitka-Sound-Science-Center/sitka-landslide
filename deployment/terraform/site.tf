49a3644261d65fe6e7b1d1ecbdb2622dfc5b6574
Bryan Quigley <code@bryanquigley.com>
2022-05-26 23:09:16 -0700

    Add recommended create_before_destroy to certificate
    This should have it create a new certificate before it tries to
    delete an old one.  No impact right now, but just a best practice.

--- deployment/terraform/site.tf
+++ deployment/terraform/site.tf
@@ -321,6 +321,10 @@ resource "aws_acm_certificate" "site" {
   provider          = aws.us-east-1
   domain_name       = var.domain_name
   validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_route53_record" "verify" {
