1e9165202fe140548670ecffac5144ae578d6518
Katie <kbrown@azavea.com>
2022-09-29 15:01:01 -0400

    Repurpose old beta banner as data quality warning banner

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,9 +1,11 @@
+import DataQualityBanner from "./DataQualityBanner";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <>
+      <DataQualityBanner />
       <Header />
       <main>{children}</main>
       <Footer />
