fe7e906a91334903299c1ff76a0106ac33abd82f
Klaas Hoekema <khoekema@azavea.com>
2022-10-05 14:24:56 -0400

    Remove weather data warning banner
    Leaving the code in case we want to reactivate it or repurpose it again in
    the future, but removing the rainfall data alert banner for now.

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,11 +1,9 @@
-import DataQualityBanner from "./DataQualityBanner";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <>
-      <DataQualityBanner />
       <Header />
       <main>{children}</main>
       <Footer />
