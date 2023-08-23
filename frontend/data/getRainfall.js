ef793b9b221580b28615d729287b0c08281591c2
Derek Dohler <ddohler@element84.com>
2023-08-23 07:13:38 -0400

    Switch to new token

--- frontend/data/getRainfall.js
+++ frontend/data/getRainfall.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const EXAGGERATION_FACTOR = process.env.EXAGGERATION_FACTOR || 1;
 
 const MESOWEST_API = "https://api.synopticdata.com/v2";
-const MESOWEST_TOKEN = process.env.MESOWEST_TOKEN || "55162f4800b34421b4a4d87491709620";
+const MESOWEST_TOKEN = process.env.MESOWEST_TOKEN || "def5d96a6f0b4316b8b331982d94105c";
 
 const NWS_API = "https://api.weather.gov/gridpoints/AJK/187,111";
 const STATION_LAT = 57.053;
